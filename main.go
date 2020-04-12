a5be3e8c110f69c8a767340974a5d2e1c617fdcf
isso0424 <isso04240129@gmail.com>
2020-04-12 14:59:08 +0900

    [Add]バグだらけだけどRoleの名前が変わったときにメッセージを送るようになった

--- main.go
+++ main.go
@@ -8,9 +8,14 @@ import (
 	"strconv"
 	"strings"
 	"syscall"
+	"time"
 
 	"github.com/bwmarrin/discordgo"
 )
+
+var roles []*discordgo.Role
+
+var tmp int
 
 func main() {
 	discord, err := discordgo.New()
@@ -22,6 +27,8 @@ func main() {
 
 	discord.AddHandler(onReady)
 	discord.AddHandler(messageCreate)
+	discord.AddHandler(createNewRole)
+	discord.AddHandler(updateRole)
 
 	err = discord.Open()
 
@@ -31,14 +38,32 @@ func main() {
 	fmt.Println("login")
 	sc := make(chan os.Signal, 1)
 	signal.Notify(sc, syscall.SIGINT, syscall.SIGTERM, os.Interrupt, os.Kill)
-	<-sc
+	tt := time.NewTicker(50 * time.Second)
+	loop := true
+
+	for loop {
+		select {
+		case <-sc:
+			loop = false
+			break
+		case <-tt.C:
+			tmp = 0
+		}
+	}
 
 	discord.Close()
 }
 
 func onReady(session *discordgo.Session, ready *discordgo.Ready) {
-	sendMessage(session, "574884574778359844", "おはよう世界")
+	//sendMessage(session, "574884574778359844", "おはよう世界")
+	sendMessage(session, "690909527461199922", "おはよう世界")
 	session.UpdateStatus(1, "Goのべんつよ、たのしいね")
+	guilds := ready.Guilds
+	if len(guilds) != 1 {
+		panic("Can't boot 2 or more bot with same token")
+	}
+	guild := guilds[0]
+	roles = guild.Roles
 }
 
 func messageCreate(s *discordgo.Session, m *discordgo.MessageCreate) {
@@ -78,17 +103,42 @@ func messageCreate(s *discordgo.Session, m *discordgo.MessageCreate) {
 	}
 }
 
+func createNewRole(session *discordgo.Session, event *discordgo.GuildRoleCreate) {
+	guild, err := session.State.Guild(event.GuildRole.GuildID)
+	if err != nil {
+		return
+	}
+	roles = guild.Roles
+	tmp = len(roles) - 1
+}
+
+func updateRole(session *discordgo.Session, event *discordgo.GuildRoleUpdate) {
+	fmt.Println(tmp)
+	if tmp != 0 {
+		tmp--
+	}
+	if tmp != 0 {
+		return
+	}
+	newRoleName := event.GuildRole.Role.Name
+	fmt.Println(event.GuildRole.Role)
+	message := "新しいクソRole***†" + newRoleName + "†***が追加されたよ"
+
+	//sendMessage(session, "574884574778359844", message)
+	sendMessage(session, "690909527461199922", message)
+}
+
 func sendMessage(session *discordgo.Session, channelID, message string) {
 	_, err := session.ChannelMessageSend(channelID, message)
 	if err != nil {
-		panic(err)
+		fmt.Println(err)
 	}
 }
 
 func loadTokenFromEnv() string {
 	fp, err := os.Open(".env")
 	if err != nil {
-		panic(err)
+		fmt.Println(err)
 	}
 
 	defer fp.Close()
